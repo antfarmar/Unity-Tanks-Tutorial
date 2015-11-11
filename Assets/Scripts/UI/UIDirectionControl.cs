b86ed478bc4c48cc2e97ad227bcc6f7700964ab1
antfarmar <markfarrugiamail@gmail.com>
2015-11-11 09:58:09 -0500

    Scripts completed, Tank prefab updated

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -1,11 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class UIDirectionControl : MonoBehaviour
 {
-    public bool m_UseRelativeRotation = true;  
+    // This class is used to make sure world space UI
+    // elements such as the health bar face the correct direction.
+
+    public bool m_UseRelativeRotation = true;       // Use relative rotation should be used for this gameobject?
 
 
-    private Quaternion m_RelativeRotation;     
+    private Quaternion m_RelativeRotation;          // The local rotatation at the start of the scene.
 
 
     private void Start()
